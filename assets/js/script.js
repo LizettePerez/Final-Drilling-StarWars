ce8566a3da35e88bdaa40d7afb9cbaf7a997db49
Lizette Pérez <lizette.perezcorrea@gmail.com>
2023-05-20 12:10:51 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,22 @@ let btnReveal1, btnReveal2, btnReveal3;
 let notGo = false;
 
 
-function* cardGeneratorRange(datos, container, start, end) {
+function* cardGeneratorRange(datos, container, start, end, color) {
   const characterToShow = datos.slice(start, end);
 
   for (const character of characterToShow) {
     yield character.name;
     container.innerHTML += `
-    <div class="col-md-2">
+    <div class="col-6 col-md-4">
       <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">${character.name}</h5>
-          <p class="card-text"><strong>Gender:</strong> ${character.gender.charAt(0).toUpperCase() + character.gender.slice(1)}</p>
-          <p class="card-text"><strong>Hair Color:</strong> ${character.hair_color.charAt(0).toUpperCase() + character.hair_color.slice(1)}</p>
-          <p class="card-text"><strong>Height:</strong> ${character.height} cm</p>
-          <p class="card-text"><strong>Weight:</strong> ${character.mass} kg</p>  
+      <div class="card-container">
+            <div class="card-icon">
+            <i class="fa-solid fa-circle fa-2xl" style="color: #${color};"></i> 
+            </div>
+          <div class="card-body">
+            <h5 class="card-title">${character.name}</h5>
+            <p class="card-text"><strong>Estatura:</strong> ${character.height} cm | <strong>Peso:</strong> ${character.mass} kg</p>
+          </div>
         </div>
       </div>
     </div>
@@ -46,11 +48,16 @@ const revealCharacter = (event, generator) => {
 document.addEventListener('DOMContentLoaded', async () => {
   cardContainer1 = document.querySelector('.cardContainer1');
   cardContainer1.innerHTML = `
-    <div class="col-md-2">
+    <div class="col-6 col-md-4">
       <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Section 1</h5>
-          <p class="card-text">You will find information about the most popular characters in the movies.</p>
+        <div class="card-container">
+            <div class="card-icon">
+            <i class="fa-solid fa-circle fa-2xl" style="color: #fa8072;"></i> 
+            </div>
+          <div class="card-body">
+            <h5 class="card-title">En esta sección...</h5>
+            <p class="card-text">Encontrarás información sobre los personajes más populares de las películas.</p>
+          </div>
         </div>
       </div>
     </div>
@@ -58,11 +65,16 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   cardContainer2 = document.querySelector('.cardContainer2');
   cardContainer2.innerHTML = `
-    <div class="col-md-2">
+    <div class="col-6 col-md-4">
       <div class="card">
+      <div class="card-container">
+            <div class="card-icon">
+            <i class="fa-solid fa-circle fa-2xl" style="color: #90ee90;"></i> 
+            </div>
         <div class="card-body">
-          <h5 class="card-title">Section 2</h5>
-          <p class="card-text">You will find information about important secondary characters.</p>
+          <h5 class="card-title">En esta sección...</h5>
+          <p class="card-text">Encontrarás información sobre personajes secundarios importantes.</p>
+        </div>
         </div>
       </div>
     </div>
@@ -70,12 +82,17 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   cardContainer3 = document.querySelector('.cardContainer3');
   cardContainer3.innerHTML = `
-    <div class="col-md-2">
+    <div class="col-6 col-md-4">
       <div class="card">
+      <div class="card-container">
+         <div class="card-icon">
+            <i class="fa-solid fa-circle fa-2xl" style="color: #87cefa;"></i> 
+          </div>
         <div class="card-body">
-          <h5 class="card-title">Section 3</h5>
-          <p class="card-text">You will find other significant characters.</p>
+          <h5 class="card-title">En esta sección...</h5>
+          <p class="card-text">Encontrarás otros personajes significativos.</p>
         </div>
+      </div>
       </div>
     </div>
   `;
@@ -89,16 +106,19 @@ document.addEventListener('DOMContentLoaded', async () => {
   btnReveal3 = document.querySelector('#btnReveal3');
   btnReveal3.addEventListener('mouseover', (event) => revealCharacter(event, gen3));
 
+  vline = document.querySelector('.v-line');
 
   datos = await fetchPeople();
   console.log('datos: ', datos);
-  gen1 = cardGeneratorRange(datos, cardContainer1, 0, 5);
-  gen2 = cardGeneratorRange(datos, cardContainer2, 5, 10);
-  gen3 = cardGeneratorRange(datos, cardContainer3, 10, 15);
+  gen1 = cardGeneratorRange(datos, cardContainer1, 0, 5, 'fa8072');
+  gen2 = cardGeneratorRange(datos, cardContainer2, 5, 10, '90ee90');
+  gen3 = cardGeneratorRange(datos, cardContainer3, 10, 15, '87cefa');
 
   btnReveal1.style.display = 'inline';
   btnReveal2.style.display = 'inline';
   btnReveal3.style.display = 'inline';
+
+  vline.style.display = 'inline';
 
   gen1.next();
   gen2.next();
@@ -108,14 +128,13 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 
 const fetchPeople = async () => {
-  let characters = [];
+  const promises = [];
 
   for (let id = 1; id <= 15; id++) {
     const url = URL_PEOPLE + id;
-    const response = await fetch(url);
-    const character = await response.json();
-    characters.push(character);
+    promises.push(fetch(url).then(response => response.json()));
   }
 
+  const characters = await Promise.all(promises);
   return characters;
 };
